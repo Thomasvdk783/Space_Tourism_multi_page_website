7a1f2afef785d34707f0c586a98aa85ef4792acf
Thomasvdk783 <thomas.vandekerchove@outlook.com>
2022-10-07 16:17:29 +0200

    finalisation destination page javascript

--- starter-code/Javascript/destination.js
+++ starter-code/Javascript/destination.js
@@ -6,97 +6,204 @@ const initDatasDestinations = async () => {
     const response = await fetchDatas()
     const destinations = response.destinations
 
-    const destinationInfos = document.getElementById('section-info-destination')
-    console.log(destinationInfos)
-    const imgDestination = document.getElementById('img-destination')
-    console.log(imgDestination)
-    const liNavDestinations = document.querySelectorAll('.li-nav-destinations')
+    const containerInfoDestination = document.getElementById('section-info-destination')
+    const containerFigureDestination = document.getElementById('container-figure')
 
-    liNavDestinations.forEach((item) => {
-        item.addEventListener('click', console.log("ok good !!"))
+    const illustrationDestination = `
+            <figure id="img-destination" class="figure-img-destination">
+            <img 
+                class="img-destination" src="${destinations[0]['images']['webp']}"
+                alt="the ${destinations[0]['name']} destination"/>
+            </figure>
+    `
+    containerFigureDestination.innerHTML = illustrationDestination
+    
+    const infoDestination = `
+            <section class="section-right-destinations">
+            <h1 class="title-destinations">${destinations[0]['name']}</h1>
+            <p class="text-description-destination">
+            ${destinations[0]['description']}
+            </p>
+            <div class="trait-destinations">
+            <!-- trait -->
+            </div>
+            <section class="container-bottom-infos-destinations">
+            <div class="container-info-destinations">
+            <h5 class="title-info-destination">AVG. DISTANCE</h5>
+            <h2 class="data-destination">${destinations[0]['distance']}</h2>
+            </div>
+            <div class="container-info-destinations">
+            <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
+            <h2 class="data-destination">${destinations[0]['travel']}</h2>
+            </div>
+            </section>
+            </section>
+        `
+    containerInfoDestination.innerHTML = infoDestination
+
+    const btnMoon = document.getElementById('moon')
+    const btnMars = document.getElementById('mars')
+    const btnEuropa = document.getElementById('europa')
+    const btnTitan = document.getElementById('titan')
+
+    btnMoon.addEventListener('click', () =>{
+        btnMoon.classList.add('active-destination')
+        btnMars.classList.remove('active-destination')
+        btnEuropa.classList.remove('active-destination')
+        btnTitan.classList.remove('active-destination')
+
+        const illustrationDestination = `
+        <figure id="img-destination" class="figure-img-destination">
+        <img 
+            class="img-destination" src="${destinations[0]['images']['webp']}"
+            alt="the ${destinations[0]['name']} destination"/>
+        </figure>
+        `
+        containerFigureDestination.innerHTML = illustrationDestination
+
+        const infoDestination = `
+        <section class="section-right-destinations">
+        <h1 class="title-destinations">${destinations[0]['name']}</h1>
+        <p class="text-description-destination">
+        ${destinations[0]['description']}
+        </p>
+        <div class="trait-destinations">
+        <!-- trait -->
+        </div>
+        <section class="container-bottom-infos-destinations">
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">AVG. DISTANCE</h5>
+        <h2 class="data-destination">${destinations[0]['distance']}</h2>
+        </div>
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
+        <h2 class="data-destination">${destinations[0]['travel']}</h2>
+        </div>
+        </section>
+        </section>
+        `
+        containerInfoDestination.innerHTML = infoDestination
     })
 
-    destinations.forEach((item) => {
-        const nameDestination = item.name
+    // Mars
+    btnMars.addEventListener('click', () => {
+        btnMoon.classList.remove('active-destination')
+        btnMars.classList.add('active-destination')
+        btnEuropa.classList.remove('active-destination')
+        btnTitan.classList.remove('active-destination')
 
+        const illustrationDestination = `
+        <figure id="img-destination" class="figure-img-destination">
+        <img 
+            class="img-destination" src="${destinations[1]['images']['webp']}"
+            alt="the ${destinations[1]['name']} destination"/>
+        </figure>
+        `
+        containerFigureDestination.innerHTML = illustrationDestination
 
-
-        
-        //<h1 class="title-destinations">MOON</h1>
-        //   <p class="text-description-destination">
-        //     See our planet as you’ve never seen it before. A perfect relaxing
-        //     trip away to help regain perspective and come back refreshed. While
-        //     you’re there, take in some history by visiting the Luna 2 and Apollo
-        //     11 landing sites.
-        //   </p>
-        //   <div class="trait-destinations">
-        //     <!-- trait -->
-        //   </div>
-        //   <section class="container-bottom-infos-destinations">
-        //     <div class="container-info-destinations">
-        //       <h5 class="title-info-destination">AVG. DISTANCE</h5>
-        //       <h2 class="data-destination">384,400 KM</h2>
-        //     </div>
-        //     <div class="container-info-destinations">
-        //       <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
-        //       <h2 class="data-destination">3 DAYS</h2>
-        //     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // <section class="section-left-destinations">
-        //   <figure class="figure-img-destination">
-        //     <img
-        //       class="img-destination"
-        //       src="./assets/destination/image-moon.webp"
-        //       alt="the moon destination"
-        //     />
-        //   </figure>
-        // </section>
-
-        // <section class="section-right-destinations">
-        //   <ul class="ul-nav-destinations">
-        //     <li class="li-nav-destinations">MOON</li>
-        //     <li class="li-nav-destinations">MARS</li>
-        //     <li class="li-nav-destinations">EUROPA</li>
-        //     <li class="li-nav-destinations">TITAN</li>
-        //   </ul>
-        //   <h1 class="title-destinations">MOON</h1>
-        //   <p class="text-description-destination">
-        //     See our planet as you’ve never seen it before. A perfect relaxing
-        //     trip away to help regain perspective and come back refreshed. While
-        //     you’re there, take in some history by visiting the Luna 2 and Apollo
-        //     11 landing sites.
-        //   </p>
-        //   <div class="trait-destinations">
-        //     <!-- trait -->
-        //   </div>
-        //   <section class="container-bottom-infos-destinations">
-        //     <div class="container-info-destinations">
-        //       <h5 class="title-info-destination">AVG. DISTANCE</h5>
-        //       <h2 class="data-destination">384,400 KM</h2>
-        //     </div>
-        //     <div class="container-info-destinations">
-        //       <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
-        //       <h2 class="data-destination">3 DAYS</h2>
-        //     </div>
-        //   </section>
-        // </section>
+        const infoDestination = `
+        <section class="section-right-destinations">
+        <h1 class="title-destinations">${destinations[1]['name']}</h1>
+        <p class="text-description-destination">
+        ${destinations[1]['description']}
+        </p>
+        <div class="trait-destinations">
+        <!-- trait -->
+        </div>
+        <section class="container-bottom-infos-destinations">
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">AVG. DISTANCE</h5>
+        <h2 class="data-destination">${destinations[1]['distance']}</h2>
+        </div>
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
+        <h2 class="data-destination">${destinations[1]['travel']}</h2>
+        </div>
+        </section>
+        </section>
+        `
+        containerInfoDestination.innerHTML = infoDestination
     })
 
+    // Europa
+    btnEuropa.addEventListener('click', () => {
+        btnMoon.classList.remove('active-destination')
+        btnMars.classList.remove('active-destination')
+        btnEuropa.classList.add('active-destination')
+        btnTitan.classList.remove('active-destination')
 
+        const illustrationDestination = `
+        <figure id="img-destination" class="figure-img-destination">
+        <img 
+            class="img-destination" src="${destinations[2]['images']['webp']}"
+            alt="the ${destinations[2]['name']} destination"/>
+        </figure>
+        `
+        containerFigureDestination.innerHTML = illustrationDestination
+
+        const infoDestination = `
+        <section class="section-right-destinations">
+        <h1 class="title-destinations">${destinations[2]['name']}</h1>
+        <p class="text-description-destination">
+        ${destinations[2]['description']}
+        </p>
+        <div class="trait-destinations">
+        <!-- trait -->
+        </div>
+        <section class="container-bottom-infos-destinations">
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">AVG. DISTANCE</h5>
+        <h2 class="data-destination">${destinations[2]['distance']}</h2>
+        </div>
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
+        <h2 class="data-destination">${destinations[2]['travel']}</h2>
+        </div>
+        </section>
+        </section>
+        `
+        containerInfoDestination.innerHTML = infoDestination
+    })
+
+    // Titan
+    btnTitan.addEventListener('click', () => {
+        btnMoon.classList.remove('active-destination')
+        btnMars.classList.remove('active-destination')
+        btnEuropa.classList.remove('active-destination')
+        btnTitan.classList.add('active-destination')
+
+        const illustrationDestination = `
+        <figure id="img-destination" class="figure-img-destination">
+        <img 
+            class="img-destination" src="${destinations[3]['images']['webp']}"
+            alt="the ${destinations[3]['name']} destination"/>
+        </figure>
+        `
+        containerFigureDestination.innerHTML = illustrationDestination
+
+        const infoDestination = `
+        <section class="section-right-destinations">
+        <h1 class="title-destinations">${destinations[3]['name']}</h1>
+        <p class="text-description-destination">
+        ${destinations[3]['description']}
+        </p>
+        <div class="trait-destinations">
+        <!-- trait -->
+        </div>
+        <section class="container-bottom-infos-destinations">
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">AVG. DISTANCE</h5>
+        <h2 class="data-destination">${destinations[3]['distance']}</h2>
+        </div>
+        <div class="container-info-destinations">
+        <h5 class="title-info-destination">EST. TRAVEL TIME</h5>
+        <h2 class="data-destination">${destinations[3]['travel']}</h2>
+        </div>
+        </section>
+        </section>
+        `
+        containerInfoDestination.innerHTML = infoDestination
+    })
 
 }
 initDatasDestinations()
